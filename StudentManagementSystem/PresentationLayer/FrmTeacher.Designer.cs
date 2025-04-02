e92c61b0983c516a223eb55299f49d3c781b4d4d
trqhuong <quynhhuongtran314@gmail.com>
2025-04-03 00:31:51 +0700

    update

--- StudentManagementSystem/PresentationLayer/FrmTeacher.Designer.cs
+++ StudentManagementSystem/PresentationLayer/FrmTeacher.Designer.cs
@@ -80,7 +80,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            
             this.pictureBox1.Location = new System.Drawing.Point(12, 71);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(444, 410);
